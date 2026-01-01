026041e943aac20f072a0d0b94970d2e44945448
Ancientmemer <ancientmemer12@gmail.com>
2026-01-01 16:18:24 +0530

    Update instagram.py

--- handlers/instagram.py
+++ handlers/instagram.py
@@ -1,12 +1,15 @@
 from pyrogram import filters
 from utils.insta import download_instagram
+import os
 
 def insta_handler(app):
     @app.on_message(filters.text & filters.regex("instagram.com"))
     async def insta(_, msg):
-        await msg.reply("⏳ Downloading Instagram media...")
+        m = await msg.reply("⏳ Downloading Instagram media...")
         file = download_instagram(msg.text)
-        if file:
+
+        if file and os.path.exists(file):
             await msg.reply_video(file)
+            os.remove(file)
         else:
-            await msg.reply("❌ Failed to download.")
+            await m.edit("❌ Failed to download Instagram media.")
