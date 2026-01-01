1067e3965a2f8dad44aac8d13b9d40ced0ab891f
Ancientmemer <ancientmemer12@gmail.com>
2026-01-01 19:41:01 +0530

    Create youtube.py

---
+++ handlers/youtube.py
@@ -0,0 +1,16 @@
+from pyrogram import filters
+from utils.yt import download_youtube
+import os
+
+def yt_handler(app):
+    @app.on_message(filters.text & filters.regex("youtube.com|youtu.be"))
+    async def yt(_, msg):
+        m = await msg.reply("⏳ Downloading YouTube video...")
+
+        file = download_youtube(msg.text)
+
+        if file and os.path.exists(file):
+            await msg.reply_video(file)
+            os.remove(file)
+        else:
+            await m.edit("❌ Failed to download YouTube video.")
