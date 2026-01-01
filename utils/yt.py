6f887e708e77ec128ff29e9e4c751903f8f61b9e
Ancientmemer <ancientmemer12@gmail.com>
2026-01-01 16:17:26 +0530

    Update yt.py

--- utils/yt.py
+++ utils/yt.py
@@ -1,13 +1,20 @@
 import yt_dlp
+import os
 
 def download_youtube(url):
+    os.makedirs("downloads", exist_ok=True)
+
     ydl_opts = {
-        'outtmpl': 'downloads/%(title)s.%(ext)s',
-        'format': 'mp4'
+        "outtmpl": "downloads/yt_%(id)s.%(ext)s",
+        "format": "bestvideo+bestaudio/best",
+        "merge_output_format": "mp4",
+        "quiet": True
     }
+
     try:
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
             info = ydl.extract_info(url, download=True)
             return ydl.prepare_filename(info)
-    except:
+    except Exception as e:
+        print("YT error:", e)
         return None
