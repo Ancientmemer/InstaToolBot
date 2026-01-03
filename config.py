7926f3aa956171ec72642d50038a4d1ca881f8e2
Ancientmemer <ancientmemer12@gmail.com>
2026-01-03 19:19:53 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,5 +3,7 @@ import os
 API_ID = int(os.getenv("API_ID"))
 API_HASH = os.getenv("API_HASH")
 BOT_TOKEN = os.getenv("BOT_TOKEN")
+MONGO_URI = os.getenv("MONGO_URI")
 
 BOT_NAME = "InstaToolBot"
+OWNER_ID = "8336106518"
