8cdce6aa6b4703157387d79d02d08d069377541a
Ancientmemer <ancientmemer12@gmail.com>
2026-01-12 22:22:55 +0530

    Change BOT_TOKEN in config.py
    Updated the BOT_TOKEN for the application.

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 API_ID = 22852603
 API_HASH = "505a27a08aac31787f203120dcbc255c"
-BOT_TOKEN = "8413622827:AAE2s-uKcaeSsIzsM0JptclynrmlItBVK-k"
+BOT_TOKEN = "8413622827:AAGCBdhZqxlYD4UpzKoVGgJFzUVBd9pLfXM"
 MONGO_URI = "mongodb+srv://Ramanan:Ramanan@cluster0.zl8rb8u.mongodb.net/?appName=Cluster0"
 
 BOT_NAME = "InstaToolBot"
